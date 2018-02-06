a5503ff28f8d83df8b6adf1dfc6bb324a62ed560
guybarnahum <guy@barnahum.com>
2018-02-05 23:22:27 -0800

    cosmetic

--- resources/views/property.blade.php
+++ resources/views/property.blade.php
@@ -20,27 +20,26 @@
     <div id="carousel-{{$property->guid}}" class="carousel slide" data-keyboard=true>
       <ol class="carousel-indicators">
         @foreach ($property->photos as $photo)
-        <li data-target="#carousel-{{$property->guid}}"
-            data-slide-to="{{$loop->index}}"
-        @if ($loop->index == 0 )
-            class="active"
-        @endif
-        ></li>
+            @if ($loop->index == 0 )
+        <li data-target="#carousel-{{$property->guid}}" data-slide-to="{{$loop->index}}" class="active" ></li>
+            @else
+        <li data-target="#carousel-{{$property->guid}}" data-slide-to="{{$loop->index}}" ></li>
+            @endif
         @endforeach
       </ol>
       <div class="carousel-inner" role="listbox">
         @foreach ($property->photos as $photo)
             @if ($loop->index == 0 )
-            <div class="carousel-item active">
+        <div class="carousel-item active">
             @else
-            <div class="carousel-item">
+        <div class="carousel-item">
             @endif
-                <div style='background:url("https://{{$photo->url}}") center center; background-size:cover;' class="slider-size">
+            <div style='background:url("https://{{$photo->url}}") center center; background-size:cover;' class="slider-size">
 {{--
 <img class="d-block img-fluid" src="https://{{$photo->url}}" alt="{{$photo->desc}}">
 --}}
-                </div>
             </div>
+        </div>
         @endforeach
       </div>
       <a class="carousel-control-prev" href="javascript:void(0)" data-target="#carousel-{{$property->guid}}" role="button" data-slide="prev">
@@ -54,7 +53,6 @@
     </div>
 
     @endif
-
 @else
 
 <!-- Property guid: N/A -->
